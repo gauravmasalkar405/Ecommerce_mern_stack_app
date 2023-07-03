20129d60ecd904bcfc2f3f3c7a49886f3d80a128
gauravmasalkar405 <125235613+gauravmasalkar405@users.noreply.github.com>
2023-07-03 22:28:35 +0530

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -10,10 +10,10 @@ const generateToken = (res, userId) => {
 
   // Set JWT as an HTTP-Only cookie
   res.cookie("jwt", token, {
-    domain: process.env.ALLOWED_ORIGIN,
+    domain: "papaya-flan-71f350.netlify.app",
     httpOnly: true, // can not be accessed by javascript on client side
     secure: process.env.NODE_ENV !== "developement", // secure flag will be active for production
-    sameSite: "none", // Setting it to "strict" ensures that the cookie is only sent in requests originating from the same site.
+    sameSite: "strict", // Setting it to "strict" ensures that the cookie is only sent in requests originating from the same site.
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
