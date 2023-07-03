3faafbc296680918523308581772c83bb4d29d3a
gauravmasalkar405 <125235613+gauravmasalkar405@users.noreply.github.com>
2023-07-03 22:20:45 +0530

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -13,7 +13,7 @@ const generateToken = (res, userId) => {
     domain: process.env.ALLOWED_ORIGIN,
     httpOnly: true, // can not be accessed by javascript on client side
     secure: process.env.NODE_ENV !== "developement", // secure flag will be active for production
-    sameSite: "Lax", // Setting it to "strict" ensures that the cookie is only sent in requests originating from the same site.
+    sameSite: "none", // Setting it to "strict" ensures that the cookie is only sent in requests originating from the same site.
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
