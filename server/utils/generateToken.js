7a5c020b6675858643a3178e28e286f636bd522d
gauravmasalkar405 <125235613+gauravmasalkar405@users.noreply.github.com>
2023-07-03 21:21:13 +0530

    changes

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const generateToken = (res, userId) => {
   // generate token
@@ -9,6 +11,7 @@ const generateToken = (res, userId) => {
   // Set JWT as an HTTP-Only cookie
   res.cookie("jwt", token, {
     httpOnly: true, // can not be accessed by javascript on client side
+    domain: process.env.ALLOWED_ORIGIN,
     secure: process.env.NODE_ENV !== "developement", // secure flag will be active for production
     sameSite: "Strict", // Setting it to "strict" ensures that the cookie is only sent in requests originating from the same site.
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
