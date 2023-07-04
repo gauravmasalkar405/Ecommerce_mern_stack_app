4fdf8c834f31e76ce65e67e8bc472dfd9ef104f7
gauravmasalkar405 <125235613+gauravmasalkar405@users.noreply.github.com>
2023-07-04 09:41:39 +0530

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -11,7 +11,7 @@ const generateToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true, // can not be accessed by javascript on client side
     secure: process.env.NODE_ENV !== "developement", // secure flag will be active for production
-    sameSite: "strict", // Setting it to "strict" ensures that the cookie is only sent in requests originating from the same site.
+    sameSite: "None", // Setting it to "strict" ensures that the cookie is only sent in requests originating from the same site.
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
